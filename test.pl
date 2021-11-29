aa0ed374defa8d0f58a149d2f181de89bca83b63
Muthu Aiswaryaa Swaminathan <withlove.aish@gmail.com>
2021-11-30 01:40:15 +0530

    the whole demo

---
+++ test.pl
@@ -0,0 +1,47 @@
+male(harry).
+
+male(ron).
+
+male(james).
+
+male(arthur).
+
+female(hermione).
+
+female(ginny).
+
+female(molly).
+
+female(lily).
+
+parent(james, harry).
+
+parent(arthur, ron).
+
+parent(molly, ron).
+
+parent(lily, harry).
+
+parent(arthur, ginny).
+
+parent(molly, ginny).
+
+mother(X, Y) :- 
+
+parent(X, Y), female(X).
+
+father(X, Y) :- 
+
+parent(X, Y), male(X).
+
+son(X, Y) :- 
+
+parent(Y, X), male(X).
+
+daughter(X, Y) :-
+
+parent(Y, X), female(X).
+
+sibling(X, Z):-
+
+mother(Y, X), mother(Y, Z).
